36447db398dd000ba76a3f3b0fcade9a818b7b42
Gagan Tiwari <gagantiwari61@gmail.com>
2016-10-14 18:15:28 +0530

    Added primary code

---
+++ index.php
@@ -0,0 +1,26 @@
+
+//code to create custom time period.
+
+add_filter('cron_schedules','my_cron_schedules');
+
+function my_cron_schedules($schedules){
+    if(!isset($schedules["15min"])){
+        $schedules["15min"] = array(
+            'interval' => 15*60,
+            'display' => __('Once every 15 minutes'));
+    }
+    return $schedules;
+}
+
+
+wp_schedule_event( time(), '15min', 'my_custom_function_action' );
+
+
+//here your function with code.
+
+add_action( 'my_custom_function_action', 'my_custom_function_code' );
+function amy_custom_function_code(){ 
+	
+  //here your magic code you want runs.
+  
+}
