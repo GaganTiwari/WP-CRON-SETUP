27105cfa2f36f04d39e3294a48f43fac2e0436a2
Gagan Tiwari <gagantiwari61@gmail.com>
2016-10-14 18:17:42 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
+<?php 
+/*
+* Gagan Tiwari
+*/
 
 //code to create custom time period.
-
 add_filter('cron_schedules','my_cron_schedules');
-
 function my_cron_schedules($schedules){
     if(!isset($schedules["15min"])){
         $schedules["15min"] = array(
@@ -13,11 +15,11 @@ function my_cron_schedules($schedules){
 }
 
 
+//schedule event here.
 wp_schedule_event( time(), '15min', 'my_custom_function_action' );
 
 
 //here your function with code.
-
 add_action( 'my_custom_function_action', 'my_custom_function_code' );
 function amy_custom_function_code(){ 
 	
